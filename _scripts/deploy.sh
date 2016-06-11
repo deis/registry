0e32047dde906a70fd351f6f578c0c831698527c
Matt Boersma <mboersma@deis.com>
2016-06-10 22:26:13 -0600

    ref(*): testing mention-bot, please ignore

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -8,5 +8,6 @@ cd "$(dirname "$0")" || exit 1
 export IMAGE_PREFIX=deisci
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 DEIS_REGISTRY='' make -C .. docker-build docker-push
+# Here is a bogus change don't you know.
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 DEIS_REGISTRY=quay.io/ make -C .. docker-build docker-push
